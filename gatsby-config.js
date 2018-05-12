e954eb8726e70814e3bbe5196455478423e58066
kindluddite <howley.phil@gmail.com>
2018-05-12 12:35:35 -0700

    fix config errors

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,17 @@ module.exports = {
   siteMetadata: {
     title: 'Better Gatsby',
   },
-  plugins: ['gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass'],
+  plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: 'onc04x4udj78',
+        accessToken:
+          '9b34a0e7bb95972ba9b0c2848e336caf753ed831b8921fdb96d097b79d2b99c3',
+      },
+    },
+    'gatsby-plugin-react-helmet',
+    'gatsby-transformer-remark',
+    'gatsby-plugin-sass',
+  ],
 }
